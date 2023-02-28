acf8ca92300f8b9da5114a02a96cfad7e3584646
tmbiro <tifanibiro@Tifanis-MacBook-Pro.local>
2023-02-28 16:01:59 -0500

    added department info

--- query.sql
+++ query.sql
@@ -32,11 +32,31 @@ ORDER BY emp_no;
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
 
-
+SELECT department.dept_no,
+	department.dept_name,
+	manager.emp_no,  
+	employee.last_name, 
+	employee.first_name
+FROM departments as department
+INNER JOIN dept_manager as manager ON
+department.dept_no = manager.dept_no
+INNER JOIN employees as employee ON
+manager.emp_no = employee.emp_no
+ORDER BY dept_no, emp_no;
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 
-
+SELECT department.dept_no,
+	designation.emp_no,  
+	employee.last_name, 
+	employee.first_name,
+	department.dept_name
+FROM departments as department
+INNER JOIN dept_emp as designation ON
+department.dept_no = designation.dept_no
+INNER JOIN employees as employee ON
+designation.emp_no = employee.emp_no
+ORDER BY dept_no, emp_no;
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
